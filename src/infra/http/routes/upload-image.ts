b0197bc347a8d5d966e7052bddbbf903124e7995
Thales Sarubbi <thalessarubbi@gmail.com>
2026-03-19 15:09:29 -0300

    docs: :memo: add swagger ui to server

--- src/infra/http/routes/upload-image.ts
+++ src/infra/http/routes/upload-image.ts
@@ -1,11 +1,27 @@
 import type { FastifyPluginAsync } from 'fastify'
+import { z } from 'zod'
 
 export const uploadImageRoute: FastifyPluginAsync = async server => {
-  server.post('/uploads', async (request, reply) => {
-    const data = request.body
-
-    // Handle image upload logic here
-
-    return reply.status(201).send({ message: 'Image uploaded successfully' })
-  })
+  server.post(
+    '/uploads',
+    {
+      schema: {
+        summary: 'Upload an image',
+        description: 'Endpoint to upload an image file',
+        body: z.object({
+          name: z.string(),
+          password: z.string().optional(),
+        }),
+        response: {
+          201: z.object({ uploadId: z.string() }),
+          409: z
+            .object({ message: z.string() })
+            .describe('Upload already exists'),
+        },
+      },
+    },
+    async (request, reply) => {
+      return reply.status(201).send({ uploadId: '<UPLOAD_ID>' })
+    }
+  )
 }
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,8 +1,12 @@
 import { env } from '@/env'
 import fastifyCors from '@fastify/cors'
+import fastifyMultipart from '@fastify/multipart'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 import fastify from 'fastify'
 import {
   hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
@@ -31,6 +35,22 @@ server.setErrorHandler((error, request, reply) => {
 })
 
 server.register(fastifyCors, { origin: '*' })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Upload Server',
+      description: 'API documentation for the upload server',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+
+server.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
 
 server.register(uploadImageRoute)
 
