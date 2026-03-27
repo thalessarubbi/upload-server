4f92ffc8613351b37486b24e55d08b8d4782fc7c
Thales Sarubbi <thalessarubbi@gmail.com>
2026-03-27 11:52:33 -0300

    feat: :sparkles: add export uploads function

---
+++ src/app/functions/export-uploads.ts
@@ -0,0 +1,82 @@
+import { PassThrough, Transform } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+import { db, pg } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { uploadFileToStorage } from '@/infra/storage/upload-file-to-storage'
+import { makeRight } from '@/shared/either'
+import type { Either } from '@/shared/either'
+import { stringify } from 'csv-stringify'
+import { ilike } from 'drizzle-orm'
+import { z } from 'zod'
+
+const ExportUploadsImageInput = z.object({
+  searchQuery: z.string().optional(),
+})
+
+type ExportUploadsImageInput = z.input<typeof ExportUploadsImageInput>
+
+type ExportUploadsOutput = {
+  reportUrl: string
+}
+
+export async function exportUploads(
+  input: ExportUploadsImageInput
+): Promise<Either<never, ExportUploadsOutput>> {
+  const { searchQuery } = ExportUploadsImageInput.parse(input)
+
+  const { sql, params } = db
+    .select({
+      name: schema.uploads.name,
+      remoteKey: schema.uploads.remoteKey,
+      remoteUrl: schema.uploads.remoteUrl,
+      createdAt: schema.uploads.createdAt,
+    })
+    .from(schema.uploads)
+    .where(
+      searchQuery ? ilike(schema.uploads.name, `%${searchQuery}%`) : undefined
+    )
+    .toSQL()
+
+  const cursor = pg.unsafe(sql, params as string[]).cursor(2)
+
+  const csv = stringify({
+    delimiter: ',',
+    header: true,
+    columns: [
+      { key: 'id', header: 'ID' },
+      { key: 'name', header: 'Name' },
+      { key: 'remote_key', header: 'Remote Key' },
+      { key: 'remote_url', header: 'Remote URL' },
+      { key: 'created_at', header: 'Created At' },
+    ],
+  })
+
+  const uploadToStorageStream = new PassThrough()
+
+  const convertToCSVPipeline = pipeline(
+    cursor,
+    new Transform({
+      objectMode: true,
+      transform(chunks: unknown[], encoding, callback) {
+        for (const chunk of chunks) {
+          this.push(chunk)
+        }
+
+        callback()
+      },
+    }),
+    csv,
+    uploadToStorageStream
+  )
+
+  const uploadToStorage = uploadFileToStorage({
+    contentType: 'text/csv',
+    folder: 'downloads',
+    fileName: `${new Date().toISOString()}-uploads.csv`,
+    contentStream: uploadToStorageStream,
+  })
+
+  const [{ url }] = await Promise.all([uploadToStorage, convertToCSVPipeline])
+
+  return makeRight({ reportUrl: url })
+}
